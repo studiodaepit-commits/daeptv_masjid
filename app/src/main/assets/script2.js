143f4df9bceaea1dad11929c8e950aea114e4d8c
studiodaepit-commits <studiodaepit@gmail.com>
2025-11-25 08:24:06 +0800

    Update script2.js

--- app/src/main/assets/script2.js
+++ app/src/main/assets/script2.js
@@ -628,8 +628,9 @@ async function initDatabase() {
         // 1. LOAD SQL.js DAN WASM (PERBAIKAN untuk Android APK)
         // ----------------------------------------------------
         const sqlPromise = initSqlJs({
-            locateFile: file => `file:///android_asset/${file}`
+            //locateFile: file => `file:///android_asset/${file}`
            // locateFile: file => `./${file}`
+            locateFile: file => file
         });
 
         SQL = await sqlPromise;
